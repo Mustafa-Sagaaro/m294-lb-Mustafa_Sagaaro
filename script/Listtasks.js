625ec131c65cf7f9268db913ff9d8bdf6634402f
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2023-04-13 13:53:00 +0200

    Task suchmöglichkeit hinzugefügt.

--- script/Listtasks.js
+++ script/Listtasks.js
@@ -1,16 +1,38 @@
- fetch('http://localhost:3000/tasks')
- .then(response => response.json())
- .then(tasks => {
-   const firstTasks = tasks.slice(0, 9);
-   firstTasks.forEach(task => {
-     const taskCard = document.createElement('div');
-     taskCard.classList.add('task-card');
-     taskCard.innerHTML = `
-       <h2>${task.title}</h2>
-       <p>Erledigt?: ${task.completed}</p>
-       <h1></h1><p>id: ${task.id}</p>`;
-     document.querySelector('.task-container').appendChild(taskCard);
-   });
- });
+function searchTasks() {
+  const searchValue = document.getElementById('search').value;
 
- 
+  fetch('http://localhost:3000/tasks')
+    .then((response) => response.json())
+    .then((tasks) => {
+      const filteredTasks = tasks.filter((task) => {
+        return (
+          task.title.toLowerCase().includes(suche.toLowerCase()) ||
+          task.id.toString().includes(suche)
+        );
+      });
+
+      Taskanzeigen(filteredTasks);
+    });
+}
+
+function Taskanzeigen(tasks) {
+  const taskContainer = document.querySelector('.taskcontainer');
+  taskContainer.innerHTML = '';
+
+  tasks.forEach((task) => {
+    const taskCard = document.createElement('div');
+    taskCard.classList.add('taskcard');
+    taskCard.innerHTML = `
+      <h2>${task.title}</h2>
+      <p>Erledigt?: ${task.completed}</p>
+      <h1></h1><p>id: ${task.id}</p>`;
+    taskContainer.appendChild(taskCard);
+  });
+}
+
+fetch('http://localhost:3000/tasks')
+  .then((response) => response.json())
+  .then((tasks) => {
+    const firstTasks = tasks.slice(0, 9);
+    Taskanzeigen(firstTasks);
+  });
